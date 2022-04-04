502690b2becad4a10949d145386e55d803fba948
Harry Purba <57930753+Harry-s-Purba@users.noreply.github.com>
2022-04-04 17:08:47 +0700

    Update soal-01.c

--- soal-01.c
+++ soal-01.c
@@ -103,7 +103,7 @@ int main()
         {
             printf("\nMasukkan Nama Buku dan Penulis Buku: ");
 	    scanf("%s",str);
-	    char ch = '\n''
+	    char ch = '\n';
 	    strncat(str,&ch,1);
 		
             token = strtok(str, ",");
